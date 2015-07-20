309cf37e1809822a0d233ac0f6fe7cda6eee22a3
crazymach <benhyee@gmail.com>
2015-07-20 16:03:22 -0700

    Deleting first sample and replacing with new sample

--- Sample/Sample/urls.py
+++ Sample/Sample/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""Sample URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
