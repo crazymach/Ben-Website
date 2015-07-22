4be03b052f41550d93516e25e5e955a41e1bc015
crazymach <benhyee@gmail.com>
2015-07-21 18:48:56 -0700

    Completion of part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list} 
-	return render(request, 'polls/index.html', context)
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
+# def index(request):
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+# 	context = {'latest_question_list': latest_question_list} 
+# 	return render(request, 'polls/index.html', context)
 
 # def detail(request, question_id): #before 404 edit
 # 	return HttpResponse("You're looking at question %s." % question_id)
@@ -20,20 +34,20 @@ def index(request):
 # 	return render(request, 'polls/ details.html', {'question': question})
 
 
-def detail(request, question_id): 			# 2nd edit of the 404
-	question = get_object_or_404(Question, pk = question_id)
-	return render(request, 'polls/detail.html' , {'question': question})
+# def detail(request, question_id): 			# 2nd edit of the 404
+# 	question = get_object_or_404(Question, pk = question_id)
+# 	return render(request, 'polls/detail.html' , {'question': question})
 
-# def results(request, question_id): #inital edit
-# 	response = "You're looking at the results of question %s."
-# 	return HttpResponse(response % question_id)
+# # def results(request, question_id): #inital edit
+# # 	response = "You're looking at the results of question %s."
+# # 	return HttpResponse(response % question_id)
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk = question_id)
-	return render(request,'polls/results.html', {'question': question})
+# def results(request, question_id):
+# 	question = get_object_or_404(Question, pk = question_id)
+# 	return render(request,'polls/results.html', {'question': question})
 
-# def vote(request, question_id): #inital vote edit
-# 	return HttpResponse("You're voting on question %s." % question_id)	
+# # def vote(request, question_id): #inital vote edit
+# # 	return HttpResponse("You're voting on question %s." % question_id)	
 
 
 def vote(request,question_id):
