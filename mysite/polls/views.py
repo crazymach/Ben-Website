6763252172d949c22a500eec4b7c0d80b709da7b
crazymach <benhyee@gmail.com>
2015-07-21 18:20:58 -0700

    Adding to detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from .models import Choice, Question
 
 
 def index(request):
@@ -26,5 +27,24 @@ def detail(request, question_id): 			# 2nd edit of the 404
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
 	return HttpResponse(response % question_id)
-def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)	
+# def vote(request, question_id): #inital vote edit
+# 	return HttpResponse("You're voting on question %s." % question_id)	
+def vote(request,question_id):
+	p = get_object_or_404(Question, pk= question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form
+		return render(request, 'polls/detail.html',{
+			'question': p, 
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes +=1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+
+
+
+
+
