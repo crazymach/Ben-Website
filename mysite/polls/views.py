d42892e2e0f7ae359f988acc78a81f5383ad563a
crazymach <benhyee@gmail.com>
2015-07-23 14:00:55 -0700

    Initial commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,9 +11,11 @@ class IndexView(generic.ListView):
 
 	def get_queryset(self):
 		return Question.objects.order_by('-pub_date')[:5]
+
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+	
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
