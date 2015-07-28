0437ced8ac71ef5d211aa0a158b100e4592c8ed8
crazymach <benhyee@gmail.com>
2015-07-28 15:56:29 -0700

    debugging

--- benhyee/benhyee/benhyee/wsgi.py
+++ benhyee/benhyee/benhyee/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benhyee.settings")
-
 application = get_wsgi_application()
